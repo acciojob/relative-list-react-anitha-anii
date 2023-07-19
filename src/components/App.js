36c8e17ffa55ec47c88071314b96f70e82f1078b
anitha-anii <anithachowdary355@gmail.com>
2023-07-19 12:36:27 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const relatives = [
+    { key: 'relativeListItem1', name: 'Uncle John' },
+    { key: 'relativeListItem2', name: 'Aunt Mary' },
+    { key: 'relativeListItem3', name: 'Cousin Alex' },
+    { key: 'relativeListItem4', name: 'Grandma Alice' },
+    { key: 'relativeListItem5', name: 'Grandpa Bob' },
+  ];
 
-export default App
+  return (
+    <div>
+      <h2>Diwali Shopping List</h2>
+      <ol>
+        {relatives.map((relative) => (
+          <li key={relative.key}>{relative.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
+
